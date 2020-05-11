e44e1316b4f80d8794449f7c0f3563638ea65a16
arnfaldur <aarnaldur@gmail.com>
2020-05-11 23:35:48 +0000

    Dr. Nim thinks, player can't cheat and game is nimer

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -26,9 +26,14 @@ import Data.Bits
 import Data.Time
 
 -- | (_,True) if it's the players turn
-type GameState = (Board,Bool)
+type GameState = (Board,Player)
 type Board = [Integer]
 type Move = (Integer,Integer)
+data Player = User | DrNim deriving Eq
+
+other :: Player -> Player
+other User = DrNim
+other DrNim = User
 
 -- This runs in a monad that can be run on the client or the server.
 -- To run code in a pure client or pure server context, use one of the
@@ -46,60 +51,72 @@ frontend = Frontend
 
       el "div" $ do
           el "p" $ text "Try to beat Dr. Nim, the master of all nimians."
-          el "p" $ text "Win by forcing your opponent to take the last bead."
+          el "p" $ text "Win by taking the last bead."
 
       --eBtn <- button "Start"
-      (elBtn, _) <- elAttr' "button" ("class" =: "button") $ text "Start"
 
-      let eBtn = domEvent Click elBtn
+      let buttonAttrs = ("class" =: "button")
+      rec
+        startButtonAttrs <- foldDyn ($) buttonAttrs . mergeWith (.) $
+          [ (\old -> old <> ("style" =: "visibility:hidden;transition-duration:1ms")) <$ eBtn
+          , (\_ -> buttonAttrs) <$ eGameOver
+          ]
+        (elBtn, _) <- do
+          (innerElBtn,_) <- elDynAttr' "button" startButtonAttrs $ text "Start"
+          return $ (innerElBtn,1)
 
-      --dBool <- toggle False (domEvent Click eBtn)
+        let eBtn = domEvent Click elBtn
+
+        dBool <- toggle False (domEvent Click elBtn)
 
       -- el "h1" $ dynText $ fmap tshow $ dBool
 
---      now <- liftIO getCurrentTime
---      eTick <- tickLossy 3 now
+        eTick <- fmap (switch . current) $ prerender (return never) $ do
+          now <- liftIO getCurrentTime
+          eTick <- tickLossy 1 now
+          return eTick
 
       --if
-      let debug = False
+        let debug = False
 
-      rec
         -- understandable AI implementation:
-        -- stateDyn :: Dynamic t GameState <-
-        --   foldDyn (\m (b,p) -> (\bo -> (nim (bestMove bo) bo, not p)) $ nim m b) (nimState,True) clickEvent
+        -- dState :: Dynamic t GameState <-
+        -- foldDyn (\m (b,p) -> (\bo -> (nim (bestMove bo) bo, not p)) $ nim m b) (nimState,True) eBeadClick
         gameText <-
           foldDyn ($) "" . mergeWith (.) $ [
               (\_ -> "") <$ eBtn
-            , (\(_, p) -> \_ -> "Game Over, You " ++ if p then "won!" else "lose") <$> eGameOver
+            , (\(_, p) -> \_ -> "Game Over, You " ++ if p == User then "have been nimed" else "are the nimest!") <$> eGameOver
           ]
 
         el "h1" $ dynText $ fmap T.pack gameText
 
-        let eStateUpdate = updated stateDyn
+        let eStateUpdate = updated dState
         let eGameOver    = ffilter (emptyBoard . fst) eStateUpdate
-        let ePlayerMoved = ffilter (not . snd) eStateUpdate
-        let eDrNimMoved  = ffilter (snd) eStateUpdate
+        let ePlayerMoved = ffilter (\p -> DrNim == snd p) eStateUpdate
+        let eDrNimMoved  = ffilter (\p -> User == snd p) eStateUpdate
 
---        eDrNimMove   <-
---              switchHold never . leftmost $ [
---                eTick <$ ePlayerMoved
---              , never <$ eStateUpdate
---              ]
+        eDrNimMove <-
+             switchHold never $ leftmost [ eTick <$ ePlayerMoved
+                                         , never <$ eStateUpdate
+                                         ]
+        ePlayerMove <-
+             switchHold never $ leftmost [ eBeadClick <$ eDrNimMoved
+                                         , never <$ eStateUpdate
+                                         ]
 
-        stateDyn :: Dynamic t GameState <-
-          foldDyn ($) ([], True) . mergeWith (.) $ [
-            fmap (\m (b,p) -> (nim m b, not p)) clickEvent
-            --, fmap (\_ (b,p) -> (nim (bestMove b) b, not p)) eDrNimMove
-            , (\_ -> (reverse [1..5], True)) <$ eBtn
-          ]
+        dState :: Dynamic t GameState <-
+          foldDyn ($) ([], User) . mergeWith (.) $ [ fmap (\m (b,p) -> (nim m b, other p)) ePlayerMove
+                                                   , fmap (\_ (b,p) -> (nim (bestMove b) b, other p)) eDrNimMove
+                                                   , (\_ -> (reverse [1..5], User)) <$ eBtn
+                                                   ]
 
-        huh :: Event t (Event t Move, Event t Move) <- networkView $ buttons <$> stateDyn
-        hoverEvent :: Event t Move <- switchHold never $ fst <$> huh
-        clickEvent :: Event t Move <- switchHold never $ snd <$> huh
-        bom <- holdDyn (0,0) $ clickEvent
-        bim <- holdDyn (0,0) $ hoverEvent
+        huh :: Event t (Event t Move, Event t Move) <- networkView $ buttons <$> dState
+        eBeadHover :: Event t Move <- switchHold never $ fst <$> huh
+        eBeadClick :: Event t Move <- switchHold never $ snd <$> huh
 
         if debug then do
+          bom <- holdDyn (0,0) $ eBeadClick
+          bim <- holdDyn (0,0) $ eBeadHover
           el "h1" $ dynText $ fmap tshow $ bom
           el "p" $ dynText $ fmap tshow $ bim
         else do return ()
@@ -123,25 +140,25 @@ buttons (nimState,player) = do
               let move = (pile, bead)
               let buttonAttrs = ("class" =: "button")
               rec
-                let hoverAttrs = (buttonAttrs <> ("style" =: "background-color:tomato")) <$ (leftmost [hoverEvent, () <$ accHover])
-                let outAttrs = buttonAttrs <$ (leftmost [outEvent, () <$ accOut])
+                let hoverAttrs = (buttonAttrs <> ("style" =: "background-color:tomato")) <$ (leftmost [eBeadHover, () <$ accHover])
+                let outAttrs = buttonAttrs <$ (leftmost [eBeadOut, () <$ accOut])
                 dynAttrs <- holdDyn buttonAttrs $ hoverAttrs <> outAttrs
-                let ( hoverEvent
-                      , outEvent
-                      , clickEvent
+                let ( eBeadHover
+                      , eBeadOut
+                      , eBeadClick
                       ) = ( domEvent Mouseover btn
                           , domEvent Mouseout btn
                           , domEvent Click btn
                           )
                 (btn,_) <- elDynAttr' "button" dynAttrs $ text "+"
-              let (finalHover,finalOut,finalClick) = bother (move <$) (hoverEvent, outEvent, clickEvent)
+              let [finalHover,finalOut,finalClick] = map (move <$) [eBeadHover, eBeadOut, eBeadClick]
               return $ ( leftmost [accHover, finalHover]
                        , leftmost [accOut, finalOut]
                        , leftmost [accClick, finalClick]
                        )
                  )
-      (hoverEvent,outEvent,clickEvent) <- foldM lamb (never,never,never) (reverse $ take (fromEnum pileSize) [1..])
-      return (hoverEvent,clickEvent)
+      (eBeadHover,_eBeadOut,eBeadClick) <- foldM lamb (never,never,never) (reverse $ take (fromEnum pileSize) [1..])
+      return (eBeadHover,eBeadClick)
     return $ both leftmost $ unzip events
 
 emptyBoard :: Board -> Bool
@@ -170,6 +187,3 @@ tshow = T.pack . show
 
 both :: (a -> b) -> (a,a) -> (b,b)
 both f (a,b) = (f a, f b)
-
-bother :: (a -> b) -> (a,a,a) -> (b,b,b)
-bother f (a,b,c) = (f a, f b, f c)
